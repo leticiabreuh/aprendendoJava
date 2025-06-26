2c9e4c202a265867e0b8f3db04d687ca43aaf1af
Letícia de Abreu <abreuhleticia@gmail.com>
2025-06-26 08:41:44 -0300

    Foi acrescentado um for, onde vai mostrar o nome do aluno, sua média e suas notas que foram digitadas

--- ArraysExercicios3Matriz.java
+++ ArraysExercicios3Matriz.java
@@ -1,41 +1,43 @@
 import java.util.Scanner;
-public class ArraysExercicios3Matriz{
-
+ 
+public class ArraysExercicios3Matriz {
+ 
     public static void main(String[] args){
-
-        String[] alunos = new String [5];
-        double[] media = new double[5];
-        double [] [] notas = new double[5][4];//Matriz5x4
-         //  double notas = 0;
-
+         String[] alunos = new String[5];
+         double[] media = new double [5];
+         double[][] notas = new double[5][4];//matriz 5*4
+         double soma=0;
         Scanner scanner = new Scanner(System.in);
-
-        for(int i = 0; i < 5; i++){
-            System.out.println("Digite o nome do aluno: "+(y+1)+" do aluno "+(i+1)+": ");
+ 
+        for(int i=0; i<5; i++){
+ 
+            System.out.println("digite o nome do aluno"+(i+1)+":");
             alunos[i] = scanner.nextLine();
-
+ 
             soma=0;
-
-            for(int y = 0; y < 4; y++){
-              System.outprintln("Digite a nota "+(y+1)+" do aluno "+alunos[i]": ");
-              notas[i][y] = scanner.nextDouble();
-              soma = soma+notas[i][y];
-
+            for (int y =0; y < 4; y++){
+                 System.out.println( "digite a nota "+(y+1)+" do aluno "+alunos[i]+":");
+                 notas[i][y] = scanner.nextDouble();
+                 soma = soma+notas[i][y];
             }
-               scanner.nextLine();//limpar o buffer
-               media[i] = notas /4;
-            
-
-            
+           
+            scanner.nextLine();//limpar o buffer
+ 
+            media[i] = soma /4;
         }
-          System.out.println("Resultados: ");
-          System.out.println("------------");
-
-
-          //Mostra o nome, media e as notas de cada aluno
-          for(int i=0; i<5; i++){
-            System.out.println("Nome: "+alunos[i]+" - Media: "+media[i]);
-          }
-
+        System.out.println("resultados: ");
+        System.out.println("------------");
+ 
+       //mostrar o nome,media e as notas de cada aluno
+        for (int i=0; i <4; i++) {
+            System.out.println("Nome: "+alunos[i]+" -media: "+media[i]);
+            System.out.println("Notas: ");
+            for(int y =0; y<4; y++){
+              System.out.println("|"+notas[i][y]+"|");
+            }
+            System.out.println("");
+            System.out.println("-------------------------------");
+        }
     }
 }
+ 
